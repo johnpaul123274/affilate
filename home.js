5bd7c392110de65fb8b24da37a3bb553574d33ed
johnpaul123274 <62330312+johnpaul123274@users.noreply.github.com>
2020-12-02 13:35:46 +0530

    update
    FGD

--- home.js
+++ home.js
@@ -7,31 +7,54 @@ document.addEventListener("DOMContentLoaded",function(){
 document.addEventListener("DOMContentLoaded",function(){
     setInterval(time,1000)
 });
-let counter=0;
-function time(){
-    counter++;
-    document.querySelector("#times").innerHTML=`reading time in sec: ${counter}`;
-    if(counter==20){
-        alert("you are lucky if got this message");
-    }
 
+// timer code
+if(!localStorage.getItem("counter")){
+    alert("welcom to jssp college");
+    localStorage.setItem("counter",0);
+}
+
+function time(){
+    var counter=localStorage.getItem("counter")
+    counter++;
+    
+
+
+
+        
+    
+    
+    document.querySelector("#times").innerHTML=`reading time in sec: ${counter}`;
+    localStorage.setItem("counter",counter);
+    
 };
-function glow(){
-    var cool= document.querySelector("h1");
-if(cool.style.visibility=="hidden"){
-    cool.style.visibility="visible"
-}
-else{
-    cool.style.visibility="hidden"
-}
+function res(){
+    localStorage.setItem("counter",0);
 };
-function glow(){
-    var cool= document.querySelector("#times");
-if(cool.style.visibility=="hidden"){
-    cool.style.visibility="visible"
+/*if(localStorage.getItem("counter",1)=1){
+    alert("welcome to jssp student hub");
 }
-else{
-    cool.style.visibility="hidden"
-}
+
+else{*/
+alert(`this is your previous study time ${localStorage.getItem("counter")} seconds`);
+window.setInterval(glow,500)
+function glow() {
+    var cool = document.querySelector("#times");
+    if (cool.style.visibility == "hidden") {
+        cool.style.visibility = "visible";
+    }
+    else {
+        cool.style.visibility = "hidden";
+    }
 };
-window.setInterval(glow,500);
+
+
+
+/*name=document.querySelector("#name").value;
+lastname=document.querySelector("#lastname".value);
+number=document.querySelector("#number").value;
+email=document.querySelector("#email").value;
+password=document.querySelector("#password").value;
+verify=document.querySelector("#verify").value;
+alert(name,lastname,number,email,password,verify);
+*/
